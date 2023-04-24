ea41c7dde234ca8989eac0ac51dd692a424213ac
shahkali <81973699+shahkali@users.noreply.github.com>
2023-04-25 06:07:40 +0800

    style:style update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-$("#accordion").on("hide.bs.collapse show.bs.collapse", e => {
-    $(e.target)
-      .prev()
-      .find("i:last-child")
-      .toggleClass("fa-minus fa-plus");
-  });
+$("#accordion").on("hide.bs.collapse show.bs.collapse", (e) => {
+  $(e.target).prev().find("i:last-child").toggleClass("fa-minus fa-plus");
+});
+$("#accordion").on("hide.bs.collapse show.bs.collapse", (e) => {
+  $(e.target).prev().find("i:last-child").toggleClass("fa-minus fa-plus");
+});
+
+function myFunction(x) {
+  if (x.matches) {
+    // If media query matches
+    document.getElementById("bdy").style.display = "none";
+    document.getElementById("alertMsg").style.display = "block";
+  } else {
+    document.getElementById("bdy").style.display = "";
+    document.getElementById("alertMsg").style.display = "none";
+  }
+}
+var windObj = window.matchMedia("(max-width: 992px)");
+myFunction(windObj); // Call listener function at run time
+windObj.addListener(myFunction);
